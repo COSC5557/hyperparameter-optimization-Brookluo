eea20c70eb07f7f1545f581f56883118377e0e8a
Yufeng Luo <brookluo@yahoo.com>
2024-02-23 22:56:14 -0700

    Distributed method works

--- hpo.jl
+++ hpo.jl
@@ -1,6 +1,6 @@
 # make sure local environment is activated
-# using Pkg
-# Pkg.activate(".")
+using Pkg
+Pkg.activate(".")
 
 using CSV
 using Plots
@@ -8,8 +8,16 @@ using MLDataUtils
 using Random
 using Evolutionary
 
-
+using Distributed
 using DataFrames
+
+# Add workers
+nprocs() == 1 && addprocs(10, exeflags=["--project=$(Base.active_project())"])
+workers()
+
+@sync @everywhere using AutoMLPipeline
+@sync @everywhere using DataFrames
+
 
 # Load the data
 df_red = CSV.read("winequality-red.csv", DataFrame)
@@ -18,35 +26,14 @@ df_red = CSV.read("winequality-red.csv", DataFrame)
 # split the data into training and testing
 Random.seed!(42)
 train, test = splitobs(shuffleobs(df_red), at = 0.7)
-global X_train = train[:, 1:end-1] 
-global Y_train = train[:,end] |> Vector
+X_train = train[:, 1:end-1] 
+Y_train = train[:,end] |> Vector
 X_test = test[:, 1:end-1]
 Y_test = test[:, end] |> Vector
 # head(x)=first(x,5)
 # head(df_red)
 
 # Define the model
-using AutoMLPipeline
-
-#### Decomposition
-pca = SKPreprocessor("PCA", Dict(:n_components=>5, :random_state=>0))
-ica = SKPreprocessor("FastICA", Dict(:n_components=>5, :whiten=>true))
-
-#### Scaler 
-rb   = SKPreprocessor("RobustScaler")
-pt   = SKPreprocessor("PowerTransformer")
-norm = SKPreprocessor("Normalizer")
-mx   = SKPreprocessor("MinMaxScaler")
-std  = SKPreprocessor("StandardScaler")
-
-#### categorical preprocessing
-ohe = OneHotEncoder()
-
-#### Column selector
-catf = CatFeatureSelector()
-numf = NumFeatureSelector()
-disc = CatNumDiscriminator()
-
 #### Learners
 rf       = hp -> SKLearner("RandomForestClassifier", n_estimators=hp[1], max_depth=hp[2], max_features=hp[3], random_state=0)
 gb       = hp -> SKLearner("GradientBoostingClassifier", n_estimators=hp[1], learning_rate=hp[2], max_depth=[3], random_state=0)
@@ -56,7 +43,26 @@ logistic = hp -> SKLearner("LogisticRegression", C=hp[1], random_state=0)
 
 
 # Define the pipeline
-function HPOLearner(learner, X, Y)
+@everywhere function HPOLearner(learner, X, Y)
+    #### Decomposition
+    pca = SKPreprocessor("PCA", Dict(:n_components=>5, :random_state=>0))
+    ica = SKPreprocessor("FastICA", Dict(:n_components=>5, :whiten=>true))
+
+    #### Scaler 
+    rb   = SKPreprocessor("RobustScaler")
+    pt   = SKPreprocessor("PowerTransformer")
+    norm = SKPreprocessor("Normalizer")
+    mx   = SKPreprocessor("MinMaxScaler")
+    std  = SKPreprocessor("StandardScaler")
+
+    #### categorical preprocessing
+    ohe = OneHotEncoder()
+
+    #### Column selector
+    catf = CatFeatureSelector()
+    numf = NumFeatureSelector()
+    disc = CatNumDiscriminator()
+
     # use evolution algorithm to find the best hyperparameters for this model
     # we will use accuracy as the metric
     # we will use 5 fold cross validation
@@ -64,34 +70,30 @@ function HPOLearner(learner, X, Y)
     # first transform the data
     # use OneHotEncoder for categorical data and RobustScaler for numerical data
     pl = @pipeline disc |> (catf |> ohe) + (numf |> rb |> pca) |> learner
-    mean, sd, _ = crossvalidate(pl, X, Y, "accuracy_score", nfolds=5, verbose=false)
-    return mean
+    crossvalidate(pl, X, Y, "accuracy_score", nfolds=5, verbose=false)
 end
 
 
 # For Random Forest
 println("Random Forest")
 # Define hypterparameter function
-HPO_rf = hp -> HPOLearner(rf(round.(Int, hp)), X_train, Y_train)
+HPO_rf = hp -> HPOLearner(rf(hp), X_train, Y_train)
 # Cannot use EA here, because inputs are all integer. Use random search instead
 # Random.seed!(42)
 x0 = [100, 10, 10]
 lower = [10, 1, 1]
-upper = [100, 30, 30]
+upper = [200, 30, 30]
 # random search
-global params = hcat([rand(lower[i]:upper[i], 100) for i=1:3]...)
+n_samp = 1000
+params = hcat([rand(lower[i]:upper[i], n_samp) for i=1:3]...)
 # tab = DataFrame(fetch.([Threads.@spawn HPO_rf(params[i, :]) for i=1:100]))
-tab = []
-println(Threads.nthreads())
-Threads.@threads for i=1:100
-    # println(Threads.threadid())
-    pl = @pipeline disc |> (catf |> ohe) + (numf |> rb |> pca) |> rf(params[i, :])
-    mean, sd, _ = crossvalidate(pl, X_train, Y_train, "accuracy_score", nfolds=5, verbose=false)
-    # HPO_rf(params[i, :])
+tab = @sync @distributed (vcat) for i=1:n_samp
+    HPO_rf(params[i, :])
 end
-# EA
+tab = DataFrame(tab)
+# GA
 # res = Evolutionary.optimize(HPO_rf, BoxConstraints(lower, upper), x0,
-#                         GA(populationSize=10, crossoverRate=0.8, mutationRate=0.4),
+#                         GA(populationSize=100, crossoverRate=0.8, mutationRate=0.4),
 #                         Evolutionary.Options(reltol=1e-4, iterations=10, show_trace=true, parallelization=:thread))
 
 # For Gradient Boosting
@@ -99,8 +101,6 @@ end
 # HPO_gb = hp -> HPOLearner(gb(hp), X_train, Y_train)
 # Cannot use EA here, because inputs are all integer. Use random search instead
 
-
-println(Threads.nthreads())
 
 # println(res)
 
