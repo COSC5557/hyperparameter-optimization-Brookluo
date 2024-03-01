c23cc21cd2933b682c56002cd55546abe4b6e33a
Yufeng Luo <brookluo@yahoo.com>
2024-02-29 20:04:45 -0700

    Finish gradient boosting, and add to store parameters and results

--- hpo.jl
+++ hpo.jl
@@ -16,6 +16,10 @@ using DataFrames
 using AutoMLPipeline
 using Random
 
+using Hyperopt
+using Plots
+using StatsPlots
+
 
 # Load the data
 df_red = CSV.read("winequality-red.csv", DataFrame)
@@ -35,9 +39,8 @@ Y_test = test[:, end] |> Vector
 # Define the model
 #### Learners
 rf       = hp -> SKLearner("RandomForestClassifier", n_estimators=hp[1], max_depth=hp[2], max_features=hp[3], random_state=0)
-gb       = hp -> SKLearner("GradientBoostingClassifier", n_estimators=hp[1], learning_rate=hp[2], max_depth=[3], random_state=0)
+gb       = hp -> SKLearner("GradientBoostingClassifier", n_estimators=Int(hp[1]), learning_rate=hp[2], max_depth=Int(hp[3]), random_state=0)
 svc      = hp -> SKLearner("SVC", C=hp[1], kernel=hp[2], degree=hp[3], random_state=0)
-ada      = hp -> SKLearner("AdaBoostClassifier", n_estimators=hp[1], learning_rate=hp[2], random_state=0)
 logistic = hp -> SKLearner("LogisticRegression", C=hp[1], random_state=0)
 
 #### Decomposition
@@ -96,8 +99,6 @@ upper = [300, 30, 30]
 #                         GA(populationSize=100, crossoverRate=0.8, mutationRate=0.4),
 #                         Evolutionary.Options(reltol=1e-4, iterations=10, show_trace=true, parallelization=:thread))
 
-using Hyperopt
-
 # random search
 println("Random Search")
 hors = @time @hyperopt for i=50,
@@ -133,22 +134,19 @@ hohbbo = @time @hyperopt for i=50,
         max_depth = lower[2]:upper[2],
         max_feature = lower[3]:upper[3]
     if state !== nothing
-        # to prevent type error and circumvent bug in their code
-        # the bug will step out of the search space
         n_est, max_depth, max_feature = round.(Int, abs.(state))
     end
     # print(i, "\n")
     if !all(lower .<= [n_est, max_depth, max_feature] .<= upper)
+        # to prevent type error and circumvent bug in their code
+        # the bug will step out of the search space
         NaN, [n_est, max_depth, max_feature]
     else
         HPO_rf([n_est, max_depth, max_feature]), [n_est, max_depth, max_feature]
     end
 end
 
-using Plots
-using StatsPlots
-
-function plot_hyperopt(ho, figname)
+function plotHyperopt(ho, figname)
     params = hcat(ho.history...)'
     vals = [t[1] for t in ho.results]
 
@@ -163,24 +161,31 @@ function plot_hyperopt(ho, figname)
     savefig(p, figname)
 end
 
-plot_hyperopt(hors, "rf_random_search.png")
-plot_hyperopt(hohb, "rf_hyperband.png")
-plot_hyperopt(hohbbo, "rf_hyperband_bo.png")
+function writeTestResult(dir, params_name, all_ho, all_cv)
+    df_cv = DataFrame(all_cv)
+    df_ho = DataFrame(hcat([ho.minimizer for ho in all_ho]...)', params_name)
+    CSV.write(dir * "all_cv.csv", df_cv)
+    CSV.write(dir * "all_ho_params.csv", df_ho)
+end
+
+plotHyperopt(hors, "../rf/rf_random_search.png")
+plotHyperopt(hohb, "../rf/rf_hyperband.png")
+plotHyperopt(hohbbo, "../rf/rf_hyperband_bo.png")
 
 # use with test data and do cross validation
 println("Cross validation")
 println("Random Sampler")
 println(hors.minimizer)
 Random.seed!(42)
-crossvalidate(rf(hors.minimizer), X_test_trans, Y_test, "accuracy_score", nfolds=5, verbose=true)
+m1 = crossvalidate(rf(hors.minimizer), X_test_trans, Y_test, "accuracy_score", nfolds=5, verbose=true)
 println("Hyperband with Random Sampler")
 println(hohb.minimizer)
 Random.seed!(42)
-crossvalidate(rf(hohb.minimizer), X_test_trans, Y_test, "accuracy_score", nfolds=5, verbose=true)
+m2 = crossvalidate(rf(hohb.minimizer), X_test_trans, Y_test, "accuracy_score", nfolds=5, verbose=true)
 println("Hyperband with BO")
 println(hohbbo.minimizer)
 Random.seed!(42)
-crossvalidate(rf(hohbbo.minimizer), X_test_trans, Y_test, "accuracy_score", nfolds=5, verbose=true)
+m3 = crossvalidate(rf(hohbbo.minimizer), X_test_trans, Y_test, "accuracy_score", nfolds=5, verbose=true)
 total_mean = [[t[1] for t in h.results] for h in [hors, hohb, hohbbo]]
 # drop missing values
 total_mean = [t[.!isnan.(t)] for t in total_mean]
@@ -188,4 +193,91 @@ p = boxplot(["Random Sampler"], -total_mean[1], ylabel="Accuracy", legend=false)
 boxplot!(p, ["Hyperband with Random Sampler"], -total_mean[2])
 boxplot!(p, ["Hyperband with BO"], -total_mean[3])
 plot!(p, legend=false, ylabel="Accuracy score")
-savefig(p, "rf_boxplot.png")
+savefig(p, "../rf/rf_boxplot.png")
+
+writeTestResult("./rf/", ["n_estimators", "max_depth", "max_feature"], [hors, hohb, hohbbo], [m1, m2, m3])
+
+function writeToCSV(filename, ho, colnames)
+    params = hcat(ho.history...)'
+    vals = [t[1] for t in ho.results]
+    CSV.write(filename, DataFrame(hcat(-vals, params), colnames))
+end
+
+writeToCSV("./rf/rf_random_search.csv", hors,
+        ["accuracy", "n_estimators", "max_depth", "max_feature"])
+writeToCSV("./rf/rf_hyperband_rs.csv", hohb,
+        ["accuracy", "n_estimators", "max_depth", "max_feature"])
+writeToCSV("./rf/rf_hyperband_bo.csv", hohbbo,
+        ["accuracy", "n_estimators", "max_depth", "max_feature"])
+
+
+# For Gradient Boosting
+println("Gradient Boosting")
+# Define hypterparameter function
+HPO_gb = hp -> HPOLearner(gb(hp), X_train_trans, Y_train)
+n_est_range = 10:500
+lr_range = LinRange(0.01, 0.5, 100)
+max_depth_range = 1:30
+
+
+# random search
+println("Random Search")
+hors = @time @hyperopt for i=50,
+        sampler = RandomSampler(rng), # This is default if none provided
+        n_est = n_est_range,
+        lr = lr_range,
+        max_depth = max_depth_range
+    # print(i, "\t", n_est, "\t", max_depth, "\t", max_feature, "   \t")
+    params = [n_est, lr, max_depth]
+    @show HPO_gb(params), params
+end
+
+# use Hyperband for optimization
+println("Hyperband")
+hohb = @time @hyperopt for i=50,
+        sampler=Hyperband(R=50, η=3, inner=RandomSampler(rng)),
+        n_est = n_est_range,
+        lr = lr_range,
+        max_depth = max_depth_range
+    if state !== nothing
+        n_est, lr, max_depth = state
+    end
+    @show HPO_gb([n_est, lr, max_depth]), [n_est, lr, max_depth]
+end
+
+# Hyperband with Bayesian optimization
+println("Hyperband with Bayesian optimization")
+hohbbo = @time @hyperopt for i=50,
+        sampler=Hyperband(R=50, η=3, inner=BOHB(dims=[Hyperopt.Continuous(), Hyperopt.Continuous(), Hyperopt.Continuous()], random_sampler=RandomSampler(rng))),
+        n_est = n_est_range,
+        lr = lr_range,
+        max_depth = max_depth_range
+    if state !== nothing
+        n_est, lr, max_depth = state
+    end
+    if !(n_est in n_est_range) || !(lr in lr_range) || !(max_depth in max_depth_range)
+        # to prevent type error and circumvent bug in their code
+        # the bug will step out of the search space
+        @show NaN, [n_est, max_depth, max_feature]
+    else
+        @show HPO_gb([n_est, lr, max_depth]), [n_est, lr, max_depth]
+    end
+end
+
+plotHyperopt(hors, "../gb/gb_random_search.png")
+plotHyperopt(hohb, "../gb/gb_hyperband.png")
+plotHyperopt(hohbbo, "../gb/gb_hyperband_bo.png")
+# cross validation
+println("Cross validation")
+println("Random Sampler")
+println(hors.minimizer)
+Random.seed!(42)
+m = crossvalidate(gb(hors.minimizer), X_test_trans, Y_test, "accuracy_score", nfolds=5, verbose=true)
+println("Hyperband with Random Sampler")
+println(hohb.minimizer)
+Random.seed!(42)
+crossvalidate(gb(hohb.minimizer), X_test_trans, Y_test, "accuracy_score", nfolds=5, verbose=true)
+println("Hyperband with BO")
+println(hohbbo.minimizer)
+Random.seed!(42)
+crossvalidate(gb(hohbbo.minimizer), X_test_trans, Y_test, "accuracy_score", nfolds=5, verbose=true)
